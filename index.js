0e65438dcefc51e29c7b942b5fb75c7ca156d20b
razvancir96 <razvan.cirlugea@gmail.ro>
2021-09-15 18:35:56 +0300

    fix syntax error and trigger prettier

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const input = document.querySelector('#cnp');
-const interpretButton = document.querySelector('#interpet-cnp');
-const resultList = document.querySelector('#result-list');
+const input = document.querySelector("#cnp");
+const interpretButton = document.querySelector("#interpet-cnp");
+const resultList = document.querySelector("#result-list");
 
 function extractGender(cnp) {
-    let genderCode = cnp.substring(1);
+  let genderCode = cnp.substring(1);
 
-    if (genderCode === 1) {
-        return 'masculin';
-    } else {
-        return 'feminin';
-    }
+  if (genderCode === 1) {
+    return "masculin";
+  } else {
+    return "feminin";
+  }
 }
 
 function extractBirthday(cnp) {
-    let day = cnp.substring(1, 3);
-    let month = cnp.substring(3, 5);
-    let year = cnp.substring(5, 7);
+  let day = cnp.substring(1, 3);
+  let month = cnp.substring(3, 5);
+  let year = cnp.substring(5, 7);
 
-    if (year < 22) {
-        year = '20' + year;
-    } else {
-        year = '19' + year;
-    }
+  if (year < 22) {
+    year = "20" + year;
+  } else {
+    year = "19" + year;
+  }
 
-    return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year}`;
 }
 
-interpretButton.addEventListener('click', function () => {
-    const inputValue = input.value;
+interpretButton.addEventListener("click", function () {
+  const inputValue = input.value;
 
-    const gender = extractGender(cnp);
-    const birthday = extractBirthday(cnp);
-    
-    resultList.innerHTML += `
-        <li>Genul: ${gender}</li>
-        <li>Data nasterii: ${birthday}</li>
-    `
-})
+  const gender = extractGender(cnp);
+  const birthday = extractBirthday(cnp);
+
+  resultList.innerHTML += `
+    <li>Genul: ${gender}</li>
+    <li>Data nasterii: ${birthday}</li>
+  `;
+});
